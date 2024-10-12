0b39dfdfc74b385f1113651651a3fd1afa8a5f52
monica pencheva <monikapeneva02@gmail.com>
2024-10-12 15:44:32 +0300

    cat testing

---
+++ app/movies.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+function movies () {
+  return (
+    <View>
+      <Text>movies</Text>
+    </View>
+  )
+}
+
+export default movies
