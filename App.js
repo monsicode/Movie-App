3d728f01410d27855d430e81577b3877e8939ca5
monica pencheva <monikapeneva02@gmail.com>
2024-10-13 16:23:52 +0300

    tree

--- App.js
+++ App.js
@@ -3,91 +3,11 @@ import { StyleSheet, Text, View , Button, Image} from 'react-native';
 import { Link, Tabs } from 'expo-router';
 import React, {useState} from 'react';
 
-// const Cat()
-
-
-
-const Cat = (props) =>{
-
-  const [isHungry, setHungry] = useState(true);
-  const [timesPet, petTheCat] = useState(0);
-
-  const catImg = timesPet > 0 ? require('./assets/happy.jpg') : require('./assets/pet.jpg')
-
-  return (
-  <View>
-    <Image
-     
-      
-      source={catImg} 
-      style={{ width:250, height:300}}
-      
-
-    />
-
-    <Text>
-      {props.name} is a cat.{props.data} { isHungry ? "Please feed me" : "thank you"}
-    </Text>
-
-      <Button onPress={()=>{
-        if(isHungry)
-        {setHungry(false);}
-         else{
-          setHungry(true);
-         }
-        }}   
-        disabled={!isHungry}
-        title={ isHungry ? "Feed the cat" :  "The cat is feed <3"}
-        />
-
-       <Text>
-        You pet the cat {timesPet} times!
-       </Text>
-
-      <Button onPress={()=>{
-            petTheCat(timesPet + 1)
-        }}   
-        title="Pet the cat"
-        />
-
-  </View>
-  )
-}
+import AppNavigation from './src/navigation';
+import CatScreen from './src/tests/catHomeScreen';
 
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-       {/* <Tabs>
-        <Tabs.Screen />
-      </Tabs> */}
-      <Text style={styles.text}>Cat cafe</Text>
-      {/* <Link href="/movies" style={{color:'blue'}}>
-          Go to movies
-      </Link> */}
-      <Cat name="Shushan"/>
-      {/* <Cat name="Baba"/> */}
-      <StatusBar style="auto" />
-    </View>
-  );
+  // return <CatScreen />
 }
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  text: {
-    fontSize:50,
-    color: 'blue',
-  },
-
-  link: {
-    color: 'blue',
-  }
-
-});
---
+++ src/tests/catHomeScreen.js
@@ -0,0 +1,86 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View , Button, Image} from 'react-native';
+
+import React, {useState} from 'react';
+
+
+const Cat = (props) =>{
+
+  const [isHungry, setHungry] = useState(true);
+  const [timesPet, petTheCat] = useState(0);
+
+ const catImg = timesPet > 0 ? require('C:\Users\Monika\Desktop\Movie_App\movie\assets\happy.jpg') : require('C:\Users\Monika\Desktop\Movie_App\movie\assets\pet.jpg')
+
+  return (
+  <View>
+    <Image
+     
+      
+      source={catImg} 
+      style={{ width:250, height:300}}
+      
+
+    />
+
+    <Text>
+      {props.name} is a cat.{props.data} { isHungry ? "Please feed me" : "thank you"}
+    </Text>
+
+      <Button onPress={()=>{
+        if(isHungry)
+        {setHungry(false);}
+         else{
+          setHungry(true);
+         }
+        }}   
+        disabled={!isHungry}
+        title={ isHungry ? "Feed the cat" :  "The cat is feed <3"}
+        />
+
+       <Text>
+        You pet the cat {timesPet} times!
+       </Text>
+
+      <Button onPress={()=>{
+            petTheCat(timesPet + 1)
+        }}   
+        title="Pet the cat"
+        />
+
+  </View>
+  )
+}
+
+
+export default function CatScreen() {
+  return (
+    <View style={styles.container}>
+
+      <Text style={styles.text}>Cat cafe</Text>
+     
+      <Cat name="Shushan"/>
+    
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  text: {
+    fontSize:50,
+    color: 'blue',
+  },
+
+  link: {
+    color: 'blue',
+  }
+
+});
